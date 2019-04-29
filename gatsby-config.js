538b58d49bae4606f372853eca54aeae68d4b10b
Jan Göger <jangoeger95@web.de>
2019-04-29 16:58:02 +0200

    master

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   pathPrefix: `/blog`,
   siteMetadata: {
-    title: `Gatsby Starter Ulrich Anders`,
+    title: `FC Rottenburg`,
     description: `This is a starter that is used for teaching at the Cologne Business School.`,
     author: `Dr. Ulrich Anders`,
     version: "0.8.0",
-    siteUrl: "https://gsua.de",
+    siteUrl: "https://gsua.de"
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,15 +13,15 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog`,
-        path: `${__dirname}/src/blog`,
-      },
+        path: `${__dirname}/src/blog`
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     {
       resolve: `gatsby-transformer-remark`,
@@ -31,20 +31,20 @@ module.exports = {
             resolve: "gatsby-remark-external-links",
             options: {
               target: "_blank",
-              rel: "noopener nofollow",
-            },
+              rel: "noopener nofollow"
+            }
           },
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 1200,
-            },
+              maxWidth: 1200
+            }
           },
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
-      },
+          `gatsby-remark-smartypants`
+        ]
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -57,49 +57,49 @@ module.exports = {
         background_color: `#343434`,
         theme_color: `#343434`,
         display: `minimal-ui`,
-        icon: `src/images/mainz.png`, // This path is relative to the root of the site.
-      },
+        icon: `src/images/mainz.png` // This path is relative to the root of the site.
+      }
     },
     {
       resolve: `gatsby-plugin-styled-components`,
       options: {
         // Add any options here
-      },
+      }
     },
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
+        pathToConfigModule: `src/utils/typography`
+      }
     },
     {
       resolve: `gatsby-plugin-sitemap`,
       options: {
         serialize: ({ site, allSitePage }) =>
           allSitePage.edges.map(edge => {
-            let path = edge.node.path
-            let priority = 0.5
+            let path = edge.node.path;
+            let priority = 0.5;
             // matching the root /
             if (path.match(/^\/$/)) {
-              priority = 1.0
+              priority = 1.0;
             } else if (path.match(/imprint/)) {
-              priority = 0.3
+              priority = 0.3;
             } else if (path.match(/data-protection/)) {
-              priority = 0.2
+              priority = 0.2;
             } else if (path.match(/sitemap/)) {
-              priority = 0.1
+              priority = 0.1;
             }
-            console.log("priority: ", priority.toFixed(1), "   path: ", path)
+            console.log("priority: ", priority.toFixed(1), "   path: ", path);
             return {
               url: site.siteMetadata.siteUrl + edge.node.path,
               changefreq: `monthly`,
-              priority,
-            }
-          }),
-      },
-    },
+              priority
+            };
+          })
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
-  ],
-}
+  ]
+};
